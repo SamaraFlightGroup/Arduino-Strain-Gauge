6a80a072030c943be3b34bc105e41bb3a34d1838
DylanTBrehm <BrehmDylan@outlook.com>
2024-09-27 14:05:50 -0500

    init

---
+++ lib/Application/Application.cpp
@@ -0,0 +1,15 @@
+#include "Application.hpp"
+
+Application::Application(/* args */)
+{
+}
+
+Application::~Application()
+{
+}
+
+void Application::runApp()
+{
+    // put your setup code here, to run once:
+    int result = myFunction(2, 3);
+}
---
+++ lib/Application/Application.hpp
@@ -0,0 +1,15 @@
+#ifndef Application_hpp
+#define Application_hpp
+
+class Application
+{
+private:
+    /* data */
+public:
+    Application(/* args */);
+    ~Application();
+
+    void runApp();
+};
+
+#endif // Application_hpp
